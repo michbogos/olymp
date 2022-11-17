40b8924b20f6a849563d820e2aec9b47a615076b
Michael Bogosta <bogostamicheal@gmail.com>
2022-11-17 20:01:07 +0100

    Update repos

---
+++ codeforces/SBM/10_09_22/x_sum.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n,m = [int(i) for i in input().split()]
+    matrix = [[] for _ in range(n)]
+    for i in range(n):
+        matrix[i] = [int(i) for i in input().split()]
+    for x in range(n):
+        for y in range(m):
+            try:
+                res = 0
+                for i in range(max(n, m)):
+            except:
+                
+    
+    print(matrix)
