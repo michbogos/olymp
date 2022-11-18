b2b24b35f6418d38696d1899a991e557cd945531
Michael Bogosta <bogostamicheal@gmail.com>
2022-11-18 14:41:53 +0100

    Remove the loose files

--- to_hex.py
+++
@@ -1,7 +0,0 @@
-HEX = "0123456789ABCDEF"
-hexnum = []
-num, base = [int(i) for i in input("Enter number and base in form of <NUMBER> <BASE>: ").split()]
-while num != 0:
-    hexnum.append(HEX[num%base])
-    print("Current: " + "".join(reversed(hexnum)),  "Rest: "+str(num%base), "Next: "+str(num//base))
-    num //= base
