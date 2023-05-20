84ff887e16b98f022e64dd504a16e2f0e2e42381
Michael Bogosta <bogostamicheal@gmail.com>
2023-05-20 18:12:23 +0200

    Update Repos

--- eolymp/geometry/line_segment_intersection.py
+++ eolymp/geometry/line_segment_intersection.py
@@ -1,18 +1,17 @@
-def line_intersect(Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2):
-    d = (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)
-    if d:
-        uA = ((Bx2 - Bx1) * (Ay1 - By1) - (By2 - By1) * (Ax1 - Bx1)) / d
-        uB = ((Ax2 - Ax1) * (Ay1 - By1) - (Ay2 - Ay1) * (Ax1 - Bx1)) / d
-    else:
-        return "No"
-    if not(0 <= uA <= 1 and 0 <= uB <= 1):
-        return "No"
-    x = Ax1 + uA * (Ax2 - Ax1)
-    y = Ay1 + uA * (Ay2 - Ay1)
-    
-    return "Yes"
+x1, y1 = [int(i) for i in input().split()]
+x2, y2 = [int(i) for i in input().split()]
+x3, y3 = [int(i) for i in input().split()]
+x4, y4 = [int(i) for i in input().split()]
 
-if __name__ == '__main__':
-    (a, b), (c, d) = (int(i) for i in input().split()), (int(i) for i in input().split())
-    (e, f), (g, h) = (int(i) for i in input().split()), (int(i) for i in input().split())
-    print(line_intersect(a, b, c, d, e, f, g, h))
+def f(x1, y1, x2, y2, x3, y3):
+    return (x2-x1)*(y3-y1)-(x3-x1)*(y2-y1)
+
+
+if (x1-x4)*(x2-x3) <= 0 and (y1-y4)*(y3-y2) <= 0:
+    if f(x1, y1, x2, y2, x3, y3)*f(x1, y1, x2, y2, x4, y4)<= 0:
+        if f(x3, y3, x4, y4, x1, y1) * f(x3, y3, x4, y4, x2, y2) <= 0:
+            print("Yes")
+            exit(0)
+
+print("No")
+
